c978e083c3cfed3e9345d1b54266839fbfa1101d
Yaoguang Zhu <mattzhu1989@gmail.com>
2020-07-22 15:35:16 +0800

    added element buffer object

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,31 @@
 #include <string>
 #include <vector>
 
-int draw(const std::vector<float>& positions);
+int draw(const std::vector<float>& positions,
+         const std::vector<unsigned int>& indices);
+
+void ProcessInput(GLFWwindow* window);
 
 std::vector<float> positions{
     -0.5f, -0.5f,
+    0.5f,  -0.5f,
     0.0f,  0.5f,
-    0.5f, -0.5f
+    1.0f,  1.0f
+};
+
+std::vector<unsigned int> indices{
+    0, 1, 2,
+    2, 1, 3
+
 };
 
 int main(){
-    int result = draw(positions);
+    int result = draw(positions, indices);
     return result;
 }
 
-int draw(const std::vector<float>& positions)
+int draw(const std::vector<float>& positions, 
+         const std::vector<unsigned int>& indices)
 {
     GLFWwindow* window;
 
@@ -36,22 +47,24 @@ int draw(const std::vector<float>& positions)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    
+    /* Initiate GLEW */
+    glewExperimental = true;
     if(glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), 
-                 positions.data(), GL_STATIC_DRAW);
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
 
-    glEnableVertexAttribArray(0);
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 
-                          sizeof(float) * 2, 0);
+    unsigned int ebo;
+    glGenBuffers(1, &ebo);
 
+    /* Generating shaders */
     unsigned int shader = shaders::CreateShader(shaders::vertex_shader, 
                                        shaders::fragment_shader);
     glUseProgram(shader);
@@ -59,10 +72,28 @@ int draw(const std::vector<float>& positions)
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        ProcessInput(window);
+
         /* Render here */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // 1. bind vertex array object
+        glBindVertexArray(vao);
+        // 2. copy our vertices array in a buffer for OpenGL to use
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), 
+                    positions.data(), GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+                    indices.data(), GL_STATIC_DRAW);
+        // 3. then set our vertex attributes pointers
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 
+                              sizeof(float) * 2, 0);
+        glEnableVertexAttribArray(0);
+        // 4. draw the object
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -73,4 +104,9 @@ int draw(const std::vector<float>& positions)
 
     glfwTerminate();
     return 0;
+}
+
+void ProcessInput(GLFWwindow* window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
