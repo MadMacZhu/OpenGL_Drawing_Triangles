c1da499e901d4e5114d206f9a95f9db53c8f3f11
Yaoguang Zhu <mattzhu1989@gmail.com>
2020-07-22 08:28:16 +0800

    Made libraries local to the project.

---
+++ build/src/CMakeFiles/bin_main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yaoguang/Dev/CppDev/OpenGL_Projs/OpenGL_Learning_Project/src/main.cpp" "/home/yaoguang/Dev/CppDev/OpenGL_Projs/OpenGL_Learning_Project/build/src/CMakeFiles/bin_main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yaoguang/Dev/CppDev/OpenGL_Projs/OpenGL_Learning_Project/build/src/CMakeFiles/GLEW.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
